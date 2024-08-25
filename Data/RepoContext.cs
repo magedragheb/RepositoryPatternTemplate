3aaf0df49d4977e8ec6e6a868c8ae56c6a871f89
Maged <19859654+magedragheb@users.noreply.github.com>
2024-08-26 00:56:42 +0300

    add dbcontext

--- Data/RepoContext.cs
+++ Data/RepoContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Data;
 
 public class RepoContext(DbContextOptions<RepoContext> options) : DbContext(options)
 {
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
 
-    // public DbSet<User> Users { get; set; }
 }
